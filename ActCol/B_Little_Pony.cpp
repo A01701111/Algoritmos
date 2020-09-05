09f8208cba1ea55025d9772ba64f8c25f1ab67fa
Sebastian R <sebas.r.2599@gmail.com>
2020-09-05 18:32:10 -0500

    Organizando carpetas

---
+++ ActCol/B_Little_Pony.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+const int sizeArr = 100000;
+
+int main(int argc, char *argv[]) {
+    int n;
+
+	std::cin>>n;
+    int arr[sizeArr];
+    for (int i = 0; i < n; i++)
+    {
+        std::cin>>arr[i];
+    }
+    for (int i = 0; i < n-1; i++)
+    {
+        /* code */
+    }
+    
+    
+	return 0;
